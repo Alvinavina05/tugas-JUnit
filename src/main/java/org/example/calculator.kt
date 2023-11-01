91f812375609e53091241163d5d565d76c243863
Alvinavina05 <alvinasugiarti@gmail.com>
2023-11-01 15:02:17 +0700

    memberikan komentar

--- src/main/java/org/example/calculator.kt
+++ src/main/java/org/example/calculator.kt
@@ -1,10 +1,16 @@
 package org.example
+// Ini adalah deklarasi paket (package) yang digunakan untuk mengelompokkan kelas 'Calculator' dalam namespace 'org.example'.
 
 class Calculator {
+    // Ini adalah deklarasi kelas 'Calculator' yang berfungsi sebagai kalkulator sederhana.
+
     fun add(first: Int, second: Int): Int {
-        return first + second
+        // Ini adalah fungsi 'add' yang digunakan untuk melakukan operasi penambahan (addisi) antara dua bilangan bulat.
+        return first + second // Mengembalikan hasil penambahan dari 'first' dan 'second'.
     }
+
     fun sub(first: Int, second: Int): Int {
-        return first - second
+        // Ini adalah fungsi 'sub' yang digunakan untuk melakukan operasi pengurangan (subtraksi) antara dua bilangan bulat.
+        return first - second // Mengembalikan hasil pengurangan 'first' dan 'second'.
     }
 }
--- src/test/java/org/example/CalculatorWithMockTest.kt
+++ src/test/java/org/example/CalculatorWithMockTest.kt
@@ -1,4 +1,5 @@
 package org.example
+// Ini adalah deklarasi paket (package) yang digunakan untuk mengelompokkan kelas 'CalculatorWithMockTest' dalam namespace 'org.example'.
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -6,29 +7,38 @@ import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
 import org.opentest4j.AssertionFailedError
 import kotlin.test.assertEquals
+// Ini adalah impor pustaka (libraries) yang digunakan dalam pengujian.
 
 class CalculatorWithMockTest {
+    // Ini adalah deklarasi kelas 'CalculatorWithMockTest' yang digunakan untuk menguji fungsi 'Calculator' dengan penggunaan objek palsu (mock).
+
     @Test
     fun `test mock calculator add method returns mocked value`() {
-        // Let's create a mock calculator
+        // Ini adalah fungsi pengujian (test) yang menguji apakah metode 'add' pada objek kalkulator palsu (mock) mengembalikan nilai yang sudah dimock.
+        // Mari kita buat objek kalkulator palsu (mock).
         val mockCalculator = mock(Calculator::class.java)
-        // Override the default behavior
+        // Mengganti perilaku default objek kalkulator palsu (mock).
         `when`(mockCalculator.add(5, 5)).thenReturn(20)
 
         val actual = mockCalculator.add(5, 5)
+        // Memanggil metode 'add' pada objek kalkulator palsu.
         println("Actual result from mock: $actual")
         assertEquals(expected = 20, actual)
+        // Memeriksa apakah hasil yang dikembalikan oleh objek kalkulator palsu sesuai dengan nilai yang dimock (20).
     }
 
     @Test
     fun `test mock calculator add method does not return a value for unhandled case`() {
-        // Let's create a mock calculator
+        // Ini adalah fungsi pengujian (test) yang menguji apakah metode 'add' pada objek kalkulator palsu (mock) tidak mengembalikan nilai untuk kasus yang tidak ditangani.
+        // Mari kita buat objek kalkulator palsu (mock).
         val mockCalculator = mock(Calculator::class.java)
-        // Override the default behavior
+        // Mengganti perilaku default objek kalkulator palsu (mock).
         `when`(mockCalculator.add(5, 5)).thenReturn(20)
 
         val actual = mockCalculator.add(5, 10)
+        // Memanggil metode 'add' pada objek kalkulator palsu.
         println("Actual result from mock: $actual")
         assertThrows<AssertionFailedError> { assertEquals(expected = 15, actual) }
+        // Memeriksa bahwa metode 'add' tidak mengembalikan nilai yang dimock (kasus yang tidak ditangani), dan ini menyebabkan pengecualian (AssertionFailedError) saat mencoba membandingkannya dengan nilai yang diharapkan (15).
     }
 }
