41356d73121dcc365f63c8bab7ececd5bcdd1ee9
Alvinavina05 <alvinasugiarti@gmail.com>
2023-11-01 12:58:02 +0700

    upload tugas JUnit

---
+++ src/test/java/org/example/CalculatorTest.kt
@@ -0,0 +1,21 @@
+package org.example
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class CalculatorTest {
+
+    @Test
+    fun add() {
+        val calculator = Calculator()
+        val expected = 10
+        assertEquals(expected, calculator.add(5, 5))
+    }
+
+    @Test
+    fun sub() {
+        val calculator = Calculator()
+        assertEquals(5, calculator.sub(15, 10))
+    }
+}
